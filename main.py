10fe7d2f7505efd4afdb542b39545ef4d33f7cb0
juanpabloquuu7-del <juanpabloquuu7@gmail.com>
2026-03-05 15:05:01 -0500

    chore(main.py): Sea creo la estructura del menu principal

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+def menu_principal():
+    while True:
+        print("\n--- SISTEMA EMPANADAS DOÑA PEPE ---")
+        print("1. Ver Menú")
+        print("2. Agregar")
+        print("3. Editar")
+        print("4. Eliminar")
+        print("5. Salir")
+
+        op = input("Seleccione: ")
+
+        if op == "1": 
+            pass
+        elif op == "2":
+            pass 
+        elif op == "3":
+            pass 
+        elif op == "4":
+            pass 
+        elif op == "5": 
+            print("Cerrando caja... ¡Adiós!")
+            break
+
+
+    menu_principal()
